06fc4d01aafdef15d3cf4ae07ab1e53d0dbbf6a5
Mo0mer <104166536+Mo0mer@users.noreply.github.com>
2022-04-21 23:04:15 +0300

    Update Счетчик хп игроков v9.py

--- Счетчик хп игроков v9.py
+++ Счетчик хп игроков v9.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint
 
-
 UserName = ''
 sus = []
 k = 0
@@ -14,6 +13,43 @@ r2 = 0
 r3 = 0
 r4 = 0
 flag = ''
+soprflag = False
+uizflag = False
+dpflag = False
+sopr = []
+uiz = []
+dpdg = []
+
+
+def dpdmg():
+    global dpflag
+    global dpdg
+    dpflag = True
+    dg = urbox.get(urbox.curselection()[0])
+    dpdg.append(dg)
+
+
+def soprf():
+    global soprflag
+    global sopr
+    soprflag = True
+    sr = elbox.get(elbox.curselection()[0])
+    sopr.append(sr)
+
+
+def uizf():
+    global uizflag
+    global uiz
+    uizflag = True
+    uz = urbox.get(urbox.curselection()[0])
+    uiz.append(uz)
+
+
+def elnabf():
+    elbox.place(x=0, y=0)
+    urbox.place(x=300, y=0)
+    soprdmg.place(x=120, y=60)
+    uizdmg.place(x=420, y=60)
 
 
 def probf():
@@ -158,11 +194,11 @@ def otk():
         bonpout.place_forget()
         cthero.place_forget()
         fzgamet.set("Фаза выбора")
-        fzgame.place(x=0, y=0+30, width=500)
-        chlistbox.place(x=110, y=100+30, width=150)
+        fzgame.place(x=0, y=0 + 30, width=500)
+        chlistbox.place(x=110, y=100 + 30, width=150)
         nametext.set("Имя персонажа")
-        name.place(x=110, y=60+30, width=150)
-        choiceclick.place(x=280, y=75+30)
+        name.place(x=110, y=60 + 30, width=150)
+        choiceclick.place(x=280, y=75 + 30)
 
 
 def reverend():
@@ -193,11 +229,11 @@ def reverend():
     revclick.place_forget()
     k = 0
     fzgamet.set("Фаза выбора")
-    fzgame.place(x=0, y=0+30, width=500)
-    chlistbox.place(x=110, y=100+30, width=150)
+    fzgame.place(x=0, y=0 + 30, width=500)
+    chlistbox.place(x=110, y=100 + 30, width=150)
     nametext.set("Имя персонажа")
-    name.place(x=110, y=60+30, width=150)
-    choiceclick.place(x=280, y=75+30)
+    name.place(x=110, y=60 + 30, width=150)
+    choiceclick.place(x=280, y=75 + 30)
     dop.place(x=0, y=0, width=100)
     pre.place(x=105, y=0, width=100)
 
@@ -230,11 +266,11 @@ def rever():
         print(*charc)
         k = 0
         fzgamet.set("Фаза выбора")
-        fzgame.place(x=0, y=0+30, width=500)
-        chlistbox.place(x=110, y=100+30, width=150)
+        fzgame.place(x=0, y=0 + 30, width=500)
+        chlistbox.place(x=110, y=100 + 30, width=150)
         nametext.set("Имя персонажа")
-        name.place(x=110, y=60+30, width=150)
-        choiceclick.place(x=280, y=75+30)
+        name.place(x=110, y=60 + 30, width=150)
+        choiceclick.place(x=280, y=75 + 30)
 
 
 def nextfaze():  # Функция для выбора
@@ -248,13 +284,13 @@ def nextfaze():  # Функция для выбора
     fzgamet.set("Фаза действия")
     nametext.set("Кол-во хила")
     bonpt.set("Бонус попадания")
-    name.place(x=285, y=50+30, width=150)
-    bonpout.place(x=65, y=50+30, width=150)
-    atackclick.place(x=65, y=140+30, width=150)
-    hilclick.place(x=285, y=140+30, width=150)
-    hl.place(x=285, y=100+30, width=150)
-    bonp.place(x=65, y=100+30, width=150)
-    fzgame.place(x=0, y=0+30, width=500)
+    name.place(x=285, y=50 + 30, width=150)
+    bonpout.place(x=65, y=50 + 30, width=150)
+    atackclick.place(x=65, y=140 + 30, width=150)
+    hilclick.place(x=285, y=140 + 30, width=150)
+    hl.place(x=285, y=100 + 30, width=150)
+    bonp.place(x=65, y=100 + 30, width=150)
+    fzgame.place(x=0, y=0 + 30, width=500)
 
 
 def hil():  # Функция исцеления
@@ -281,11 +317,11 @@ def hil():  # Функция исцеления
     hl.place_forget()
     bonp.place_forget()
     fzgamet.set("Фаза выбора")
-    fzgame.place(x=0, y=0+30, width=500)
-    chlistbox.place(x=110, y=100+30, width=150)
+    fzgame.place(x=0, y=0 + 30, width=500)
+    chlistbox.place(x=110, y=100 + 30, width=150)
     nametext.set("Имя персонажа")
-    name.place(x=110, y=60+30, width=150)
-    choiceclick.place(x=280, y=75+30)
+    name.place(x=110, y=60 + 30, width=150)
+    choiceclick.place(x=280, y=75 + 30)
 
 
 def mybtnclick():  # Функция нажатия на кнопку ОК
@@ -295,13 +331,14 @@ def mybtnclick():  # Функция нажатия на кнопку ОК
     hptext.set("ХП героя")
     kdtext.set("КД героя")
     fzgamet.set("Фаза создания")
+    elnab.place(x=370, y=150)
     fzgame.place(x=0, y=30, width=500)
-    name.place(x=50, y=70+20, width=100)
-    hpout.place(x=200, y=70+20, width=100)
-    kdout.place(x=350, y=70+20, width=100)
-    namehero.place(x=50, y=100+20, width=100)
-    hp.place(x=200, y=100+20, width=100)
-    kd.place(x=350, y=100+20, width=100)
+    name.place(x=50, y=70 + 20, width=100)
+    hpout.place(x=200, y=70 + 20, width=100)
+    kdout.place(x=350, y=70 + 20, width=100)
+    namehero.place(x=50, y=100 + 20, width=100)
+    hp.place(x=200, y=100 + 20, width=100)
+    kd.place(x=350, y=100 + 20, width=100)
     revnab.place(x=245, y=180, width=120)
     endnab.place(x=370, y=180, width=120)
     dop.place(x=0, y=0, width=100)
@@ -324,15 +361,25 @@ def choiceplace():  # Функция нажатия на кнопку Закон
     kd.place_forget()
     revnab.place_forget()
     endnab.place_forget()
+    soprdmg.place_forget()
+    uizdmg.place_forget()
+    elbox.place_forget()
+    elnab.place_forget()
     fzgamet.set("Фаза выбора")
     fzgame.place(x=0, y=30, width=500)
-    chlistbox.place(x=110, y=100+30, width=150)
+    chlistbox.place(x=110, y=100 + 30, width=150)
     nametext.set("Имя персонажа")
-    name.place(x=110, y=60+30, width=150)
-    choiceclick.place(x=280, y=75+30)
+    name.place(x=110, y=60 + 30, width=150)
+    choiceclick.place(x=280, y=75 + 30)
+    urbox.place(x=250, y=0)
+    dpdmg.place(x=0, y=0)
 
 
 def nabhero():  # Функция для создания героев
+    global sopr
+    global uiz
+    global soprflag
+    global uizflag
     global k
     global char
     global charc
@@ -346,9 +393,24 @@ def nabhero():  # Функция для создания героев
     st.append(nh)
     st.append(hph)
     st.append(kdh)
+    if soprflag and uizflag is False:
+        st.append('| Сопр -')
+        st.append(sopr)
+    elif uizflag and soprflag is False:
+        st.append('| Уяз -')
+        st.append(uiz)
+    elif soprflag is True and uizflag is True:
+        st.append('| Сопр -')
+        st.append(sopr)
+        st.append('| Уяз -')
+        st.append(uiz)
     print(*st)
     char.append(st)
     charc.append(st)
+    sopr = []
+    uiz = []
+    uizflag = False
+    soprflag = False
     nameinp.set('')
     hpinp.set('')
     kdinp.set('')
@@ -406,14 +468,14 @@ def prdamage():  # Функция для опеределения пробити
         kritt.set('Урон будет увеличен вдвое')
         if flag:
             krit.place(x=125, y=65, width=250)
-        name.place(x=10, y=70+30, width=150)  # Кол-во костей(текст)
-        kdout.place(x=175, y=70+30, width=150)  # Вид кости(текст)
-        hpout.place(x=340, y=70+30, width=150)  # Доп урон(текст)
-        fzgame.place(x=0, y=0+30, width=500)
-        namehero.place(x=10, y=100+30, width=150)  # Кол-во костей
-        hp.place(x=175, y=100+30, width=150)  # Вид кости
-        kd.place(x=340, y=100+30, width=150)  # Доп урон
-        dmgclick.place(x=0, y=150+30, width=500)  # Кнопка бам
+        name.place(x=10, y=70 + 30, width=150)  # Кол-во костей(текст)
+        kdout.place(x=175, y=70 + 30, width=150)  # Вид кости(текст)
+        hpout.place(x=340, y=70 + 30, width=150)  # Доп урон(текст)
+        fzgame.place(x=0, y=0 + 30, width=500)
+        namehero.place(x=10, y=100 + 30, width=150)  # Кол-во костей
+        hp.place(x=175, y=100 + 30, width=150)  # Вид кости
+        kd.place(x=340, y=100 + 30, width=150)  # Доп урон
+        dmgclick.place(x=0, y=150 + 30, width=500)  # Кнопка бам
     else:
         messagebox.showinfo('Счетчик хп', 'Рикошет')
         name.place_forget()
@@ -424,15 +486,17 @@ def prdamage():  # Функция для опеределения пробити
         bonp.place_forget()
         krit.place_forget()
         fzgamet.set("Фаза выбора")
-        fzgame.place(x=0, y=0+30, width=500)
-        chlistbox.place(x=110, y=100+30, width=150)
+        fzgame.place(x=0, y=0 + 30, width=500)
+        chlistbox.place(x=110, y=100 + 30, width=150)
         nametext.set("Имя персонажа")
-        name.place(x=110, y=60+30, width=150)
-        choiceclick.place(x=280, y=75+30)
+        name.place(x=110, y=60 + 30, width=150)
+        choiceclick.place(x=280, y=75 + 30)
 
 
 def damage():
     global flag
+    global dpflag
+    global dpdg
     cd, dice, diceb = int(namehero.get()), int(hp.get()), int(kd.get())
     dmg = 0
     for i in range(cd):
@@ -441,9 +505,17 @@ def damage():
         dmg += int(diceb)
     if flag:
         dmg = dmg * 2
+    if dpflag:
+        if dpdg[0] in els:
+            messagebox.showinfo(sus[0], 'Сопротивление')
+            dmg = int(dmg / 2)
+        elif dpdg[0] in ur:
+            messagebox.showinfo(sus[0], 'Уязвимость')
+            dmg = dmg * 2
     sus[1] = sus[1] - dmg
-    char[index]=sus
+    char[index] = sus
     flag = False
+    dpflag = False
     messagebox.showinfo(sus[0], 'Нанесенный урон: ' + str(dmg))
     print('-----------------------------------')
     print('Нанесенный урон: ', dmg)
@@ -487,55 +559,60 @@ def damage():
         kd.place_forget()
         dmgclick.place_forget()
         fzgamet.set("Фаза выбора")
-        fzgame.place(x=0, y=0+30, width=500)
-        chlistbox.place(x=110, y=100+30, width=150)
+        fzgame.place(x=0, y=0 + 30, width=500)
+        chlistbox.place(x=110, y=100 + 30, width=150)
         nametext.set("Имя персонажа")
-        choiceclick.place(x=280, y=75+30)
-        name.place(x=110, y=60+30, width=150)
+        choiceclick.place(x=280, y=75 + 30)
+        name.place(x=110, y=60 + 30, width=150)
         for i in char:
             chlistbox.insert(END, i)
         print(*char)
+        print(dpdg)
+        dpdg = []
 
 
 root = Tk()  # Главное окно
 root.title('Счетчик хп')
 root.geometry('500x220+300+200')
 
+root1 = Tk()  # Главное окно
+root1.title('Счетчик хп')
+root1.geometry('500x220+400+300')
 
-MyBtn=Button(root, text="Start", font='Times 40', bg="#FF0000", fg='#FFFFFF', width=3, command=mybtnclick)  # Кнопка
+MyBtn = Button(root, text="Start", font='Times 40', bg="#FF0000", fg='#FFFFFF', width=3, command=mybtnclick)  # Кнопка
 MyBtn.place(x=200, y=50)
 
-nametext=StringVar()
-hptext=StringVar()
-kdtext=StringVar()
-bonpt=StringVar()
-ctherot=StringVar()
-fzgamet=StringVar()
-kritt=StringVar()
+nametext = StringVar()
+hptext = StringVar()
+kdtext = StringVar()
+bonpt = StringVar()
+ctherot = StringVar()
+fzgamet = StringVar()
+kritt = StringVar()
 
-name=Label(root, textvariable=nametext, fg="#eee", bg="#20B2AA", font='Times 14')  # Имя героя(текст)
-hpout=Label(root, textvariable=hptext, fg="#eee", bg="#20B2AA", font='Times 14')  # Хп героя(текст)
-kdout=Label(root, textvariable=kdtext, fg="#eee", bg="#20B2AA", font='Times 14')  # Кд героя(текст)
-bonpout=Label(root, textvariable=bonpt, fg="#eee", bg="#20B2AA", font='Times 14')  # Бонус к попаданию(текст)
-cthero=Label(root, textvariable=ctherot, fg="#eee", bg="#20B2AA", font='Times 14')  # Кол-во героев(текст)
-fzgame=Label(root, textvariable=fzgamet, fg="#eee", bg="#20B2AA", font='Times 14')  # Фаза игры
-krit=Label(root, textvariable=kritt, fg="#eee", bg="#20B2AA", font='Times 14')
+name = Label(root, textvariable=nametext, fg="#eee", bg="#20B2AA", font='Times 14')  # Имя героя(текст)
+hpout = Label(root, textvariable=hptext, fg="#eee", bg="#20B2AA", font='Times 14')  # Хп героя(текст)
+kdout = Label(root, textvariable=kdtext, fg="#eee", bg="#20B2AA", font='Times 14')  # Кд героя(текст)
+bonpout = Label(root, textvariable=bonpt, fg="#eee", bg="#20B2AA", font='Times 14')  # Бонус к попаданию(текст)
+cthero = Label(root, textvariable=ctherot, fg="#eee", bg="#20B2AA", font='Times 14')  # Кол-во героев(текст)
+fzgame = Label(root, textvariable=fzgamet, fg="#eee", bg="#20B2AA", font='Times 14')  # Фаза игры
+krit = Label(root, textvariable=kritt, fg="#eee", bg="#20B2AA", font='Times 14')
 
-nameinp=StringVar()
-hpinp=StringVar()
-kdinp=StringVar()
-bonpinp=StringVar()
-hilinp=StringVar()
-iskdinp=StringVar()
+nameinp = StringVar()
+hpinp = StringVar()
+kdinp = StringVar()
+bonpinp = StringVar()
+hilinp = StringVar()
+iskdinp = StringVar()
 
-ism=Entry(root, textvariable=iskdinp)
-bonp=Entry(root, textvariable=bonpinp)  # Ввод бонуса попадания
-namehero=Entry(root, textvariable=nameinp)  # Вводи имени героя
-hp=Entry(root, textvariable=hpinp)  # Ввод хп героя
-kd=Entry(root, textvariable=kdinp)  # Ввод кд героя
-hl=Entry(root, textvariable=hilinp)  # Ввод хила
+ism = Entry(root, textvariable=iskdinp)
+bonp = Entry(root, textvariable=bonpinp)  # Ввод бонуса попадания
+namehero = Entry(root, textvariable=nameinp)  # Вводи имени героя
+hp = Entry(root, textvariable=hpinp)  # Ввод хп героя
+kd = Entry(root, textvariable=kdinp)  # Ввод кд героя
+hl = Entry(root, textvariable=hilinp)  # Ввод хила
 
-revnab=Button(root, text="Добавить героя", font='Times 12', fg="#20B2AA", command=nabhero)  # Кнопка +герой
+revnab = Button(root, text="Добавить героя", font='Times 12', fg="#20B2AA", command=nabhero)  # Кнопка +герой
 endnab = Button(root, text='Закончить набор', font='Times 12', fg='#20B2AA', command=choiceplace)  # Кнопка завершения
 atackclick = Button(root, text='Атака', font='Times 18', fg='#20B2AA', command=prdamage)  # Кнопка ОК
 dmgclick = Button(root, text='Бам!', font='Times 18', fg='#20B2AA', command=damage)  # Кнопка нанесения урона
@@ -552,8 +629,26 @@ ismkdpl = Button(root, text='+', font='Times 11', fg='#FFFFFF', bg='#1E90FF', co
 ismkdmn = Button(root, text='-', font='Times 11', fg='#FFFFFF', bg='#1E90FF', command=minus)
 prob = Button(root, text='➴', font='Times 11', fg='#FFFFFF', bg='#1E90FF', command=probf)
 atk = Button(root, text='Доп.атака', font='Times 11', fg='#20B2AA', command=atkf)
+elnab = Button(root, text='Доб.хар.', font='Times 11', fg='#20B2AA', command=elnabf)
+soprdmg = Button(root1, text='Доб.сопр', font='Times 11', fg='#20B2AA', command=soprf)
+uizdmg = Button(root1, text='Доб.уязв', font='Times 11', fg='#20B2AA', command=uizf)
+dpdmg = Button(root1, text='Доб.урон', font='Times 11', fg='#20B2AA', command=dpdmg)
 
 chlistbox = Listbox(root, height=3, width=20, selectmode=SINGLE)  # Список с героями
+
+urbox = Listbox(root1, height=12, width=20, selectmode=SINGLE)
+elbox = Listbox(root1, height=12, width=20, selectmode=SINGLE)
+
+els = ['1.Колющий', '2.Рубящий', '3.Дробящий', '4.Кислотный', '5.Холодом', '6.Огненный', '7.Силовой', '8.Молнией',
+       '9.Некротический', '10.Ядовитый', '11.Психический', '12.Свет']
+ur = ['1.Колющий', '2.Рубящий', '3.Дробящий', '4.Кислотный', '5.Холодом', '6.Огненный', '7.Силовой', '8.Молнией',
+      '9.Некротический', '10.Ядовитый', '11.Психический', '12.Свет']
+
+for q in els:
+    elbox.insert(END, q)
+
+for y in ur:
+    urbox.insert(END, y)
 
 tp = IntVar()
 tp.set(10)
