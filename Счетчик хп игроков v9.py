21a38517697bd4919b71135a390ce9eade7c6bd0
Okawaiikot0 <104271786+Okawaiikot0@users.noreply.github.com>
2022-04-23 23:11:54 +0300

    Update Счетчик хп игроков v9.py

--- Счетчик хп игроков v9.py
+++ Счетчик хп игроков v9.py
@@ -579,7 +579,7 @@ root1 = Tk()  # Главное окно
 root1.title('Счетчик хп')
 root1.geometry('500x220+400+300')
 
-MyBtn = Button(root.configure(bg='#4D2100'), text="Start", font='Times 40', bg="#FFB474", fg='#000000', width=3, command=mybtnclick)  # Кнопка
+MyBtn = Button(root.configure(bg='#371900'), text="Start", font='Times 40', bg="#FFE3CC", fg='#000000', width=3, command=mybtnclick)  # Кнопка
 MyBtn.place(x=200, y=50)
 
 nametext = StringVar()
